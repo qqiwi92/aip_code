24b41ad3fe0db01b2254a4bb86cb328846b6a7e1
qiwi <qiwi@qiwi.qiwi>
2025-11-05 13:33:20 +0300

    cg

--- theory_27.10.cpp
+++ theory_27.10.cpp
@@ -1,6 +1,5 @@
 #include <cstddef>
 #include <iostream>
-#include <limits>
 
 using t = std::size_t;
 
@@ -9,7 +8,7 @@ void sort(int *arr, t len) {
   while (flag) {
     flag = false;
     for (t i = 0; i + 1 < len; i++) {
-      if (arr[i] > arr[i+1]) {
+      if (arr[i] > arr[i + 1]) {
         flag = true;
         int temp = arr[i];
         arr[i] = arr[i + 1];
@@ -23,19 +22,17 @@ int main() {
   t len = 0;
   std::cin >> len;
   int *array = new int[len];
-
   int input = 0;
   for (t i = 0; i < len; i++) {
     std::cin >> input;
     array[i] = input;
   }
-  sort(array,len);
+  sort(array, len);
   if (len % 2 == 0) {
     double sum = array[len / 2] + array[len / 2 - 1];
     std::cout << sum / 2 << "\n";
   } else {
     std::cout << array[len / 2] << "\n";
   }
-
   delete[] array;
 }
